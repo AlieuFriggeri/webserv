c4f3e8465392b6b92086fac85430fb6b0888818c
Alieu Friggeri <afrigger@c2r3s7.42lausanne.ch>
2023-09-25 13:53:10 +0200

    gitignore ratio

---
+++ webserv/headers/webserv.hpp
@@ -0,0 +1,48 @@
+#ifndef WEBSERV_HPP
+#define WEBSERV_HPP
+
+#include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string.h>
+#include <poll.h>
+#include <sys/event.h>
+#include <sys/time.h>
+
+
+class Server{
+
+
+	public:
+	Server();
+	~Server();
+	void setup(int port);
+	void handleConnection(int clientsocket);
+
+
+	int _listening_socket;
+	sockaddr_in _server;
+	fd_set _current_socket, _ready_socket;
+	char _svc[NI_MAXSERV];
+	char _buffer[4096];
+	private:
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif
